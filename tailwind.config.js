b14f75b883439238e6eb3ee9e31993bcdd9c2330
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-20 16:37:12 +0100

    css change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
-module.exports = {
-  // ... other config
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
   daisyui: {
     themes: ['light'], // Only use light theme
     darkTheme: false, // Disable dark theme detection
