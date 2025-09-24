da2e52aca1e18b2356cab10b386498618e6199c3
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-09-24 18:23:25 +0100

    test hero

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -20,6 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-// export const analytics = getAnalytics(app) 
+export const analytics = getAnalytics(app) 
 export const storage = getStorage(app)
 export const db = getFirestore(app)
